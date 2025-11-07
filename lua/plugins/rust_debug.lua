a927eb3104d16d2ded0fb8d1d43f48bade1b7b8c
MikeHuntThe3rd <103815901+MikeHuntThe3rd@users.noreply.github.com>
2025-11-07 20:42:27 +0100

    Update rust_debug.lua

--- lua/plugins/rust_debug.lua
+++ lua/plugins/rust_debug.lua
@@ -1,17 +1,5 @@
 return {
   {
-    'mrcjkb/rustaceanvim',
-    version = '^6', -- Recommended
-    lazy = false, -- This plugin is already lazy
-  },
-  {
-    'rust-lang/rust.vim',
-    ft = "rust",
-    init = function()
-      vim.g.rustfmt_autosave = 1
-    end
-  },
-  {
     'saecki/crates.nvim',
     ft = { "toml" },
     config = function()
@@ -32,4 +20,5 @@ return {
     end
   }
 }
+
 
