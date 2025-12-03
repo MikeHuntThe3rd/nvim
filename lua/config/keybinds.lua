c6524cda15ce3859f495e3116bfe2a082fef6dc9
MikeHuntThe3rd <103815901+MikeHuntThe3rd@users.noreply.github.com>
2025-12-03 23:12:49 +0100

    Update keybinds.lua

--- lua/config/keybinds.lua
+++ lua/config/keybinds.lua
@@ -1,10 +1,11 @@
 vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>cd", vim.cmd.Ex)
+vim.keymap.set("n", "<leader>q", vim.cmd.wq)
 vim.keymap.set("n", "<leader>ss", function()
   vim.cmd("wall")  -- Save all open files
   print("All files saved")
 end, { desc = "Save all files" })
 vim.keymap.set("n", "<leader>qq", function()
-  vim.cmd("qall")  -- Save all open files
   vim.cmd("wall") 
+  vim.cmd("qall")  -- Save all open files
 end, { desc = "Save all files" })
